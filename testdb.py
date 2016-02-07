08d18746a6bf5a0359c198c60b4526fdf769bce1
Djibril Keita <djibrilhms@gmail.com>
2016-02-07 05:24:02 +0000

    tested transfer

--- testdb.py
+++ testdb.py
@@ -1,8 +1,10 @@
 import os
 import unittest
+import stripe
 from flask import Flask
 from models import User,Base,db,Card
-
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 
 app = Flask(__name__)
@@ -10,17 +12,94 @@ app = Flask(__name__)
 class TestCase(unittest.TestCase):
 
     def setUp(self):
+        stripe.api_key = 'sk_test_OM2dp9YnI2w5eNuUKtrxd56g'
         db.drop_all()
         db.create_all()
 
     def addCard(self):
         u1 = User(first_name='Djibril',email='Djibrilhms@gmail.com', password='xyzhv')
-        c1 = Card(CardNumber='4000000000000077',expYear='2017',expMonth='12',User_id=u1.id)
+        c1 = Card(CardNumber='4000056655665556',expYear='2017',expMonth='12',User_id=u1.id)
         db.session.add(c1)
         db.session.add(u1)
         db.session.commit()
         self.assertIsNotNone(c1)
 
+    def testTransfers(self):
+        u1 = User(first_name='Djibril',email='Djibril@gmail.com', password='xyzhv')
+        c1 = Card(CardNumber='4000056655665556',expYear='2017',expMonth='12',User_id=u1.id)
+        u2 = User(first_name='Djibril',email='Djibril@live.com', password='xyzhv')
+        c2 = Card(CardNumber='4000056655665556',expYear='2018',expMonth='10',User_id=u2.id)
+        db.session.add(c1)
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        
+        token1 = stripe.Token.create(
+            card={
+                "number":c1.CardNumber,
+                "exp_month":c1.expMonth,
+                "exp_year": c1.expYear,
+                'default_for_currency' : 'true',
+                'currency' : 'usd'
+                },
+                )
+        
+        token2 = stripe.Token.create(
+            card={
+                "number":c1.CardNumber,
+                "exp_month":c1.expMonth,
+                "exp_year": c1.expYear,
+                'default_for_currency' : 'true',
+                'currency' : 'usd'
+                },
+                )
+        token3 = stripe.Token.create(
+            card={
+                "number":c2.CardNumber,
+                "exp_month":c2.expMonth,
+                "exp_year": c2.expYear,
+                'default_for_currency' : 'true',
+                'currency' : 'usd'
+                },
+                )     
+        self.assertIsNotNone(token1.id)
+        self.assertIsNotNone(token2.id)
+        
+        cus1 = stripe.Customer.create(
+        description="Customer for test@example.com",
+        source=token1.id
+        )
+        ##setting up transfer
+        stpacc1 = stripe.Account.create(
+        country='US',
+        managed=True,
+        email = 'djibril@gmail.com',
+        external_account = token2.id,
+        )
+        stpacc2 = stripe.Account.create(
+        country='US',
+        managed=True,
+        email = 'djibril@gmail.com',
+        external_account = token3.id,
+        )
+        self.assertIsNotNone(cus1.id)
+        ##creating charge
+        charge = stripe.Charge.create(
+            description="test.py",
+            amount = '3000',
+            currency = "usd",
+            customer= cus1.id
+            )
+        
+        transfer= stripe.Transfer.create(
+        amount=400,
+        currency="usd",
+        destination = stpacc2.id,
+        source_transaction = charge.id,
+        description="Transfer for test@example.com"
+        )
+        
+        # self.assertIsNotNone(transfer.id)
 
     def testUser(self):
         u1 = User(first_name='Djibril',email='Djibrilhms@gmail.com', password='xyzhv')
